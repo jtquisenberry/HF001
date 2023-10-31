c78da0af0d3faa1ed5ce288922bb61c96cd681f8
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2023-10-30 20:52:05 -0500

    cola

--- orca/app.py
+++ orca/app.py
@@ -1,4 +1,5 @@
 # Use a pipeline as a high-level helper
+import torch
 from transformers import pipeline
 
 # Load the model on GPU #0 memory
@@ -7,9 +8,10 @@ from transformers import pipeline
 # Load the model on CPU memory
 # model = pipeline("text-generation", model="Open-Orca/Mistral-7B-OpenOrca", device="cpu").model
 # Cast to half precision.
-model = pipeline("text-generation", model="Open-Orca/Mistral-7B-OpenOrca", device="cpu").model.half().half()
+# model = pipeline("text-generation", model="Open-Orca/Mistral-7B-OpenOrca", device="cpu").model.half().half()
+model = pipeline("text-generation", model="Open-Orca/Mistral-7B-OpenOrca", device="cpu", torch_dtype=torch.float16).model
 # Save locally.
-model.save_pretrained(r"D:\model_quarter.mdl")
+model.save_pretrained(r"D:\model_f16.mdl")
 
 # pipe = pipeline("text-generation", model="Open-Orca/Mistral-7B-OpenOrca")
 a = 1
