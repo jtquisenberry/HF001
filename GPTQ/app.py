027760cb00838e048b3e5c1bef6cd4da221171b6
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2023-11-01 20:04:21 -0500

    TheBloke/Dolphin-Llama2-7B-GPTQ

--- GPTQ/app.py
+++ GPTQ/app.py
@@ -9,7 +9,7 @@ from transformers import pipeline
 # model = pipeline("text-generation", model="Open-Orca/Mistral-7B-OpenOrca", device="cpu").model
 # Cast to half precision.
 # model = pipeline("text-generation", model="Open-Orca/Mistral-7B-OpenOrca", device="cpu").model.half().half()
-model = pipeline("text-generation", model="Open-Orca/LlongOrca-7B-16k", device="cpu", torch_dtype=torch.float16).model
+model = pipeline("text-generation", model="TheBloke/Dolphin-Llama2-7B-GPTQ", device="cuda:0", torch_dtype=torch.float16).model
 # Save locally.
 model.save_pretrained(r"D:\model_f16.mdl")
 
