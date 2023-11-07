0ec696ef274c1c114ee53daa065d6db216ef082d
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2023-11-06 21:24:31 -0600

    multiple

--- orca/app.py
+++ orca/app.py
@@ -2,6 +2,13 @@
 import torch
 from transformers import pipeline
 
+torch.device(0)
+device = torch.cuda.current_device()
+properties = torch.cuda.get_device_properties(device=device)
+print("Current Device")
+print(properties)
+print()
+
 # Load the model on GPU #0 memory
 # pipe = pipeline("text-generation", model="Open-Orca/Mistral-7B-OpenOrca", device="cuda:0")
 
@@ -9,9 +16,11 @@ from transformers import pipeline
 # model = pipeline("text-generation", model="Open-Orca/Mistral-7B-OpenOrca", device="cpu").model
 # Cast to half precision.
 # model = pipeline("text-generation", model="Open-Orca/Mistral-7B-OpenOrca", device="cpu").model.half().half()
-model = pipeline("text-generation", model="Open-Orca/LlongOrca-7B-16k", device="cpu", torch_dtype=torch.float16).model
+# model = pipeline("text-generation", model="Open-Orca/LlongOrca-7B-16k", device="cpu", torch_dtype=torch.float16).model
+model = pipeline("text-generation", model="Open-Orca/LlongOrca-7B-16k", device=0, torch_dtype=torch.float16).model
+model = pipeline("text-generation", model="Open-Orca/LlongOrca-7B-16k", device=0).model
 # Save locally.
-model.save_pretrained(r"D:\model_f16.mdl")
+# model.save_pretrained(r"D:\model_f16.mdl")
 
 # pipe = pipeline("text-generation", model="Open-Orca/Mistral-7B-OpenOrca")
 a = 1
---
+++ quantization/app.py
@@ -0,0 +1,2 @@
+# https://towardsdatascience.com/introduction-to-weight-quantization-2494701b9c0c
+
